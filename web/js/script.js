e85c7e06f1c347b9656ab577096a3fc3517dd5e4
Peter Thompson <peter.thompson@dunelm.org.uk>
2014-09-07 21:36:48 +0200

    Add about page

--- web/js/script.js
+++ web/js/script.js
@@ -2,6 +2,7 @@ angular.module('country-interface', ['ngRoute', 'ci.countries', 'ci.maps']).
 
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.
+        when('/about', {templateUrl: 'template/about.html'}).
         when('/country/:cca3', {controller: 'CountryDetailController', templateUrl: 'template/countryDetail.html'}).
         when('/search/:search', {controller: 'CountryIndexController', templateUrl: 'template/countryIndex.html'}).
         otherwise({controller: 'CountryIndexController', templateUrl: 'template/countryIndex.html'});
